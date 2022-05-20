8d8171d8b9762db6f83e41b00f1a9e406cecce4f
david <davidjhoyjr@gmail.com>
2022-05-19 21:54:00 -0400

    commit tree e08d448d59d640eeb8453f50ee959c732a6f890a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, name){
+    const usedName = name.toUpperCase()
+    return arr.filter(item => item.toUpperCase() === usedName)
+}
+
+function fuzzyMatch(arr, partial){
+    return arr.filter(item => item.slice(0,partial.length)=== partial)
+}
+
+function matchName(arr, name){
+    return arr.filter(item => item.name === name);
+}
